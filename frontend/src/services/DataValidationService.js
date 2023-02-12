579ac5f00e43452247f73fb587443631e8794242
JeeEssEm <greendiesel228@gmail.com>
2023-02-12 18:43:01 +0300

    валидация пароля и логина

---
+++ frontend/src/services/DataValidationService.js
@@ -0,0 +1,41 @@
+
+export default class DataValidationService {
+    static validatePassword(password, repeatedPassword) {
+        const regexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+        if (password !== repeatedPassword) {
+            return [
+                false,
+                'Пароли не совпадают'
+            ]
+        }
+
+        if (regexp.test(password)) {
+            return [
+                true,
+                 ''
+            ]
+        }
+        return [
+            false,
+            'Пароль должен содержать минимум одну,' +
+                ' цифру одну букву, и длина должна быть не меньше 8 символов'
+        ]
+    }
+
+    static validateLogin(login) {
+        if (login.length < 2 || login.length > 31) {
+            return [
+                false,
+                'Длина логина должна быть не меньше 2 символов и' +
+                    ' не больше 32 символов'
+            ]
+        }
+
+        return [
+            true,
+            ''
+        ]
+    }
+}
+
